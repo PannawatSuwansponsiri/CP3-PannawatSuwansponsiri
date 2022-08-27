4461a3f416e8e1071460d7206d3a6ee20908b7ff
PannawatSuwansponsiri <111620463+PannawatSuwansponsiri@users.noreply.github.com>
2022-08-27 12:16:43 +0700

    Exam

---
+++ Variable.py
@@ -0,0 +1,9 @@
+FE = 86
+GB = 79.8
+ITCS = 99.1
+CP= 89
+print("-----Your Score-------")
+print("Foundation English",":",FE)
+print("General Business",":",GB)
+print("Introduction to Computer Systems",":",ITCS)
+print("Computer Programming",":",CP)
