8e6b11a2d9f0a96422b9f21d1e14036ec9688b0e
PannawatSuwansponsiri <111620463+PannawatSuwansponsiri@users.noreply.github.com>
2022-10-08 15:36:36 +0700

    Add files via upload

---
+++ While.py
@@ -0,0 +1,6 @@
+username = input("Username :")
+password = input("Password :")
+while  username != "admin" or password != "1234":
+    username = input("Username :")
+    password = input("Password :")
+print("Done!")
